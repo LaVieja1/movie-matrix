b6558359a203c5b1a36a6afa4491c5e940231694
Santi <canosantiago404@gmail.com>
2024-03-18 11:42:34 -0300

    fixed width

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 export default async function Home() {
   const res = await fetch(
-    "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US&page=1",
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
   const data = await res.json();
@@ -14,26 +14,16 @@ export default async function Home() {
   return (
     <main className="min-h-screen">
       <Header />
-      <div className="py-16 px-8">
-        {/*
+      <div className="py-16 px-8 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {results.map((movie: any) => (
           <Card
             key={movie.id}
             image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             title={movie.title}
-            description={movie.overview}
             year={movie.release_date}
             rating={movie.vote_average}
           />
         ))}
-        */}
-        <Card
-          key={movie.id}
-          image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          title={movie.title}
-          year={movie.release_date}
-          rating={movie.vote_average}
-        />
       </div>
     </main>
   );
--- components/Card.tsx
+++ components/Card.tsx
@@ -11,12 +11,17 @@ interface CardProps {
 
 const Card = ({ image, title, year, rating }: CardProps) => {
   return (
-    <div className="bg-slate-800 flex flex-col items-center justify-center w-[250px] p-4 text-white rounded-md">
+    <div className="bg-slate-800 flex flex-col items-center justify-evenly w-full p-2 text-white rounded-md text-center">
       <h1 className="text-xl">{title}</h1>
-      <p>{year}</p>
-      <p className="text-yellow-500">{rating}</p>
+      <p className="text-slate-400">{year}</p>
+      <p className="text-yellow-500">{rating.toPrecision(2)}</p>
       <div className="relative aspect-square w-[95%]">
-        <Image src={image} alt={title} className="aspect-square" fill />
+        <Image
+          src={image}
+          alt={title}
+          className="aspect-square rounded-md"
+          fill
+        />
       </div>
     </div>
   );
