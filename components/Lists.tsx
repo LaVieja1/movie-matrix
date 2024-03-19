931f19c6552f98c9cbeb0c6034bdfde0d77ff8fb
Santi <canosantiago404@gmail.com>
2024-03-19 11:15:46 -0300

    añadidas listas por distinto tipo

--- components/Lists.tsx
+++ components/Lists.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "next/navigation";
 import ListItem from "./ListItem";
 import {
   NavigationMenu,
@@ -8,6 +9,9 @@ import {
 } from "./ui/navigation-menu";
 
 const Lists = () => {
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type");
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -16,10 +20,21 @@ const Lists = () => {
             Listas
           </NavigationMenuTrigger>
           <NavigationMenuContent className="flex flex-col items-start gap-y-2 text-sm px-8 py-4">
-            <ListItem title="Populares" param="popular" />
-            <ListItem title="Mejor puntación" param="top_rated" />
-            <ListItem title="En cartelera" param="now_playing" />
-            <ListItem title="Proximamente" param="upcoming" />
+            {type === "movie" ? (
+              <>
+                <ListItem title="Populares" param="popular" />
+                <ListItem title="Mejor puntación" param="top_rated" />
+                <ListItem title="En cartelera" param="now_playing" />
+                <ListItem title="Proximamente" param="upcoming" />
+              </>
+            ) : (
+              <>
+                <ListItem title="Populares" param="popular" />
+                <ListItem title="Mejor puntación" param="top_rated" />
+                <ListItem title="En el aire" param="on_the_air" />
+                <ListItem title="Nuevo capitulo" param="airing_today" />
+              </>
+            )}
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
