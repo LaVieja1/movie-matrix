94318fe9adcfb3754c16f25ba5ffc51ac84f2d6f
Santi <canosantiago404@gmail.com>
2024-03-21 14:54:39 -0300

    añadida custom not found

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>No se encontro la página</h1>
+    </div>
+  );
+};
+
+export default NotFound;
